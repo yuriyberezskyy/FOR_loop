e47b4ffea1b6457bd8114e75f10690b3c615f93b
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-10 23:05:33 +0000

    range()->function

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
   print(fruit)
+
+for number in range(1,10):
+  print(number)
+
+sum = 0
+for number in range(1,101):
+  sum+=number
+
+print(sum)
